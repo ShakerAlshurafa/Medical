f9efaea9616ae16e5c4340054e8946b324d71b3e
Shaker Yousef <shakrshrfa@gmail.com>
2023-12-29 17:21:53 +0200

    first commit

---
+++ assets/js/home/main.js
@@ -0,0 +1,15 @@
+// // start questions section
+// var content = document.querySelectorAll('.question-content');
+
+// for (var i = 0; i < content.length; i++) {
+//     var ind= i;
+//     content[i].addEventListener('click', function (e) {
+//         console.log(ind , content[ind]);
+//         var question = document.querySelector('.question-p');
+//         question.classList.toggle('d-none');
+//         // var border = document.querySelectorAll('.question-border');
+//         // border[i].classList.toggle('d-none');
+//     });
+// }
+
+// // end questions section
